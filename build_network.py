Fixed population names in bkg_weights_population.csv; fixed lgn-to-v1 syn_weight normalization.

build_network.py
@@ -256,6 +256,7 @@ def syn_weight_by_experimental_distribution(
         orient_temp = 1 - (delta_tuning_180 / 180)
         orient_temp = np.min([0.999, orient_temp])
         orient_temp = np.max([0.001, orient_temp])
+
         syn_weight = weight_rv.ppf(orient_temp)
         n_syns_ = 1
 
@@ -513,7 +514,8 @@ def fake(source, target):
 
 
 def lgn_synaptic_weight_rule(source, target, base_weight, mean_size):
-    return base_weight / mean_size * target["target_sizes"]
+    # return base_weight / mean_size * target["target_sizes"]
+    return base_weight / target["target_sizes"] * mean_size
 
 
 def add_lgn_v1_edges(v1_net, lgn_net, x_len=240.0, y_len=120.0):