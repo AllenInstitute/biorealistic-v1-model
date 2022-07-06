06dfe5a420d5d5faf9411659bb923f7a02e21df6
dhaufler <haufler@gmail.com>
2022-07-06 12:21:08 -0700

    updated to use multiple synapse types for background connections (same synapse models/unitary weights as LGN projections)

--- make_bkg_weights.py
+++ make_bkg_weights.py
@@ -3,14 +3,44 @@
 import pandas as pd
 import json
 import numpy as np
+import pop_names
 
 
-v1unitary = json.load(open("base_props/OLD_v1_synapse_amps.json", "r"))
-v1unitary_ser = pd.Series(v1unitary["e2e"], name="Unitary PSP")
-v1unitary_ser_i = pd.Series(v1unitary["e2i"], name="Unitary PSP")
-v1unitary_ser.index = pd.to_numeric(v1unitary_ser.index)
-v1unitary_ser_i.index = pd.to_numeric(v1unitary_ser_i.index)
-v1unitary_ser_both = v1unitary_ser.append(v1unitary_ser_i)
+v1unitary = json.load(open("precomputed_props/v1_synapse_amps.json", "r"))
+v1unitary_ser_e23 = pd.Series(v1unitary["lgn_to_e23"], name="Unitary PSP")
+v1unitary_ser_e4 = pd.Series(v1unitary["lgn_to_e4"], name="Unitary PSP")
+v1unitary_ser_e5et = pd.Series(v1unitary["lgn_to_e5et"], name="Unitary PSP")
+v1unitary_ser_e5it = pd.Series(v1unitary["lgn_to_e5it"], name="Unitary PSP")
+v1unitary_ser_e5np = pd.Series(v1unitary["lgn_to_e5np"], name="Unitary PSP")
+v1unitary_ser_e6 = pd.Series(v1unitary["lgn_to_e6"], name="Unitary PSP")
+v1unitary_ser_pv = pd.Series(v1unitary["lgn_to_pv"], name="Unitary PSP")
+v1unitary_ser_sst = pd.Series(v1unitary["lgn_to_sst"], name="Unitary PSP")
+v1unitary_ser_vip = pd.Series(v1unitary["lgn_to_vip"], name="Unitary PSP")
+
+v1unitary_ser_e23.index = pd.to_numeric(v1unitary_ser_e23.index)
+v1unitary_ser_e4.index = pd.to_numeric(v1unitary_ser_e4.index)
+v1unitary_ser_e5et.index = pd.to_numeric(v1unitary_ser_e5et.index)
+v1unitary_ser_e5it.index = pd.to_numeric(v1unitary_ser_e5it.index)
+v1unitary_ser_e5np.index = pd.to_numeric(v1unitary_ser_e5np.index)
+v1unitary_ser_e6.index = pd.to_numeric(v1unitary_ser_e6.index)
+v1unitary_ser_pv.index = pd.to_numeric(v1unitary_ser_pv.index)
+v1unitary_ser_sst.index = pd.to_numeric(v1unitary_ser_sst.index)
+v1unitary_ser_vip.index = pd.to_numeric(v1unitary_ser_vip.index)
+v1unitary_ser_e23
+
+v1unitary_ser = pd.concat(
+    [
+        v1unitary_ser_e23,
+        v1unitary_ser_e4,
+        v1unitary_ser_e5et,
+        v1unitary_ser_e5it,
+        v1unitary_ser_e5np,
+        v1unitary_ser_e6,
+        v1unitary_ser_pv,
+        v1unitary_ser_sst,
+        v1unitary_ser_vip,
+    ]
+)
 
 # also load up population weight
 
@@ -22,13 +52,14 @@ pop_model_dict = {}
 for loc, locdict in v1_node_model["locations"].items():
     for pop, popdict in locdict.items():
         for m in popdict["models"]:
+
             pop_model_dict[m["node_type_id"]] = pop
 
 # %%
 
 pop_df = pd.DataFrame.from_dict(pop_model_dict, orient="index", columns=["population"])
 pop_df["model_id"] = pop_df.index
-pop_psp = pop_df.merge(v1unitary_ser_both, left_index=True, right_index=True)
+pop_psp = pop_df.merge(v1unitary_ser, left_index=True, right_index=True)
 
 
 pop_bkg = pop_psp.merge(pop_weights, on="population")
